08da77d8ff7e5a0f95156c30931cc0d853d7e185
principle105 <principle105@gmail.com>
2023-08-12 21:45:06 -0400

    glow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,211 @@
 
 <Header />
 
+<div class="absolute inset-0 -z-50">
+    <svg
+        width="1440"
+        height="738"
+        viewBox="0 0 1440 738"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g opacity="0.6" filter="url(#filter0_f_61_80)">
+            <rect
+                x="93.217"
+                y="-109.893"
+                width="917.84"
+                height="144"
+                rx="72"
+                transform="rotate(42.6061 93.217 -109.893)"
+                fill="url(#paint0_linear_61_80)"
+            />
+        </g>
+        <g opacity="0.6" filter="url(#filter1_f_61_80)">
+            <rect
+                x="750"
+                y="438.246"
+                width="1182.42"
+                height="144"
+                rx="72"
+                transform="rotate(-51.335 750 438.246)"
+                fill="url(#paint1_linear_61_80)"
+            />
+        </g>
+        <g opacity="0.6" filter="url(#filter2_f_61_80)">
+            <rect
+                x="960"
+                y="348.764"
+                width="1182.42"
+                height="144"
+                rx="72"
+                transform="rotate(-19.5034 960 348.764)"
+                fill="url(#paint2_linear_61_80)"
+            />
+        </g>
+        <g opacity="0.6" filter="url(#filter3_f_61_80)">
+            <rect
+                x="525.686"
+                y="527.542"
+                width="1182.42"
+                height="144"
+                rx="72"
+                transform="rotate(-161.404 525.686 527.542)"
+                fill="url(#paint3_linear_61_80)"
+            />
+        </g>
+        <defs>
+            <filter
+                id="filter0_f_61_80"
+                x="-124.53"
+                y="-230.159"
+                width="1013.57"
+                height="967.856"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+            >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                />
+                <feGaussianBlur
+                    stdDeviation="75"
+                    result="effect1_foregroundBlur_61_80"
+                />
+            </filter>
+            <filter
+                id="filter1_f_61_80"
+                x="629.199"
+                y="-605.801"
+                width="1092.77"
+                height="1254.81"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+            >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                />
+                <feGaussianBlur
+                    stdDeviation="75"
+                    result="effect1_foregroundBlur_61_80"
+                />
+            </filter>
+            <filter
+                id="filter2_f_61_80"
+                x="829.887"
+                y="-176.113"
+                width="1422.87"
+                height="790.727"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+            >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                />
+                <feGaussianBlur
+                    stdDeviation="75"
+                    result="effect1_foregroundBlur_61_80"
+                />
+            </filter>
+            <filter
+                id="filter3_f_61_80"
+                x="-725.818"
+                y="-116.818"
+                width="1428.24"
+                height="775.179"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+            >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                />
+                <feGaussianBlur
+                    stdDeviation="75"
+                    result="effect1_foregroundBlur_61_80"
+                />
+            </filter>
+            <linearGradient
+                id="paint0_linear_61_80"
+                x1="298.183"
+                y1="-62.4687"
+                x2="1026.68"
+                y2="-20.9483"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop offset="0.20236" stop-color="#AA92E5" />
+                <stop
+                    offset="0.693473"
+                    stop-color="#AA92E5"
+                    stop-opacity="0.74"
+                />
+                <stop offset="1" stop-color="#AA92E5" stop-opacity="0" />
+            </linearGradient>
+            <linearGradient
+                id="paint1_linear_61_80"
+                x1="1542.82"
+                y1="566.564"
+                x2="473.556"
+                y2="419.023"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop offset="0.20236" stop-color="#AA92E5" />
+                <stop
+                    offset="0.693473"
+                    stop-color="#AA92E5"
+                    stop-opacity="0.74"
+                />
+                <stop offset="1" stop-color="#AA92E5" stop-opacity="0" />
+            </linearGradient>
+            <linearGradient
+                id="paint2_linear_61_80"
+                x1="1752.82"
+                y1="477.083"
+                x2="683.556"
+                y2="329.542"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop offset="0.20236" stop-color="#929AE5" />
+                <stop
+                    offset="0.693473"
+                    stop-color="#929AE5"
+                    stop-opacity="0.74"
+                />
+                <stop offset="1" stop-color="#929AE5" stop-opacity="0" />
+            </linearGradient>
+            <linearGradient
+                id="paint3_linear_61_80"
+                x1="1318.5"
+                y1="655.86"
+                x2="249.242"
+                y2="508.319"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop offset="0.20236" stop-color="#929AE5" />
+                <stop
+                    offset="0.693473"
+                    stop-color="#929AE5"
+                    stop-opacity="0.74"
+                />
+                <stop offset="1" stop-color="#929AE5" stop-opacity="0" />
+            </linearGradient>
+        </defs>
+    </svg>
+</div>
+
 <section class="flex flex-col mt-10">
     <div class="mx-auto text-center relative">
         <div class="absolute -z-50 top-0 h-5/6 overflow-hidden">
